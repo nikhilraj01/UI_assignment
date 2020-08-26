114a1087eacc82e1e86e43324c1328943f16208d
Nikhil Raj <44263051+nikhilraj01@users.noreply.github.com>
2020-08-26 12:34:15 +0530

    Update main.js
    Added- Feature to open images in the same tab; Navigation using enter and backspace; fixed footer; fixed footer location

--- main.js
+++ main.js
@@ -8,117 +8,157 @@ var lastSelRow;
 var empty;
 var months=["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
-    function initFolders(path="/") { currentPath=path;
-        fetch("http://localhost:9000/filemanager?mode=readfolder&path="+path)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function (response) {
-            var folderTable = document.getElementById("fmListFolders");  
-            folderTable.innerHTML="";
-            var table=document.createElement("table");
-            var head1 = document.createElement("th");                   
-            var head2 = document.createElement("th");
-            head1.textContent="Name";
-            head2.textContent="Date Modified";
-            var hrow=document.createElement("tr");
-            hrow.appendChild(head1);
-            hrow.appendChild(head2);
-            table.appendChild(hrow);
-            var num=0;       
-            if (Array.isArray(response.data)) {
-                response.data.forEach(element => {
-                    var row=document.createElement("tr");                   
-                    nm = document.createElement("td");  
-                    mod = document.createElement("td");
-                    var path=element.attributes.path;
-                    row.addEventListener("dblclick", function()
+function initFolders(path="/") { currentPath=path;
+    fetch("http://localhost:9000/filemanager?mode=readfolder&path="+path)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function (response) {
+        var folderTable = document.getElementById("fmListFolders");  
+        folderTable.innerHTML="";
+        var table=document.createElement("table");        
+        var head1 = document.createElement("th");                   
+        var head2 = document.createElement("th");
+        head1.textContent="Name";
+        head2.textContent="Date Modified";
+        var hrow=document.createElement("tr");
+        hrow.appendChild(head1);
+        hrow.appendChild(head2);
+        table.appendChild(hrow);
+        var num=0;       
+        if (Array.isArray(response.data)) {
+            response.data.forEach(element => {
+                var row=document.createElement("tr");                   
+                nm = document.createElement("td");  
+                mod = document.createElement("td");
+                var path=element.attributes.path;                    
+                row.addEventListener("dblclick", function()
+                {
+                    var img=path.substring(path.length-3,path.length);
+                    if(img.toUpperCase()=="PNG" || img.toUpperCase()=="JPG" )
+                    {
+                        var modal = document.getElementById("Modal");
+                        modal.style.display = "block";
+                        var modalImg = document.getElementById("img");
+                        var imgCaption = document.getElementById("caption");
+                        imgCaption.innerHTML = path;
+                        modalImg.src = "http://localhost:9000/filemanager?mode=getimage&path="+path+"&thumbnail=true";
+                        var span = document.getElementsByClassName("close")[0];
+                        span.onclick = function()
+                        {
+                            modal.style.display = "none";
+                        }
+                    }
+                    else
                     {
                         initFolders(path);
-                    });
-                    row.addEventListener("keypress", function(e)
-                        {
-                            if ( e.keyCode == 13 ){
-                            initFolders(path);
-                        }});                    
-                    row.addEventListener("contextmenu", (e)=>
-                    {
-                        fileName=e.target.textContent;
-                        selectedFilePath=currentPath+fileName;
-                        e.preventDefault();
-                        showContextMenu();
-                        cm=document.getElementById("contextMenu");
-                        cm.style.top=e.clientY;
-                        cm.style.left=e.clientX;
-                    });
-                    row.addEventListener("click" , (e)=>
-                    {
-                        if(lastSelRow){
-                            lastSelRow.style.opacity=1;
-                            lastSelRow.style.border="black";
-                            lastSelRow.style.backgroundColor="white";
-                        }
-                        var onsel=document.getElementById("onselect");
-                        onsel.style.opacity=1.0;
-                        row.style.border="grey 3px solid";
-                        row.style.backgroundColor="rgba(190, 190, 190, 0.692)";
-                        fileName=e.target.textContent;
-                        selectedFilePath=currentPath+fileName;
-                        lastSelRow=row;    
-                    });  
-                    nm.textContent=element.attributes.name;
-                    mod.textContent=months[element.attributes.modified.substring(6,7)-1]+" "+element.attributes.modified.substring(8,10)+", "+element.attributes.modified.substring(0,4);
-                    row.appendChild(nm);
-                    row.appendChild(mod);
-                    table.appendChild(row);
+                    }
                 });
-            }
-            if(table.children.length==1){
-                empty="true";
-                var row=document.createElement("tr");                   
-                nm = document.createElement("td"); 
-                nm.textContent="The folder is empty";
-                nm.colSpan=2;
-                row.appendChild(nm);
-                table.appendChild(row);
+                row.addEventListener("keypress", function(e)
+                {
+                    if ( e.keyCode == 13 ){
+                        var img=path.substring(path.length-3,path.length);
+                        if(img.toUpperCase()=="PNG" || img.toUpperCase()=="JPG" )
+                        {                                
+                            window.open("http://localhost:9000/filemanager?mode=getimage&path="+path+"&thumbnail=true", "_target");
+                        }
+                        else
+                        {
+                            initFolders(path);
+                        }
+                    }
+                });
                 row.addEventListener("contextmenu", (e)=>
+                {
+                    fileName=e.target.textContent;
+                    selectedFilePath=currentPath+fileName;
+                    e.preventDefault();
+                    showContextMenu();
+                    cm=document.getElementById("contextMenu");
+                    cm.style.top=e.clientY;
+                    cm.style.left=e.clientX;                
+                });
+                row.addEventListener("click" , (e)=>
+                {
+                    if(lastSelRow)
                     {
-                        fileName=e.target.textContent;
-                        selectedFilePath=currentPath+fileName;
-                        e.preventDefault();
-                        showContextMenu();
-                        cm=document.getElementById("contextMenu");
-                        cm.style.top=e.clientY;
-                        cm.style.left=e.clientX;
-                    });   
-            }
-            folderTable.appendChild(table);
-        });
-        var dirList=document.getElementById("dirList");
-        dirList.innerHTML="";
-        var res = currentPath.split("/");  
-        var path;
-        var str="";
-        var pathArr=[];
-        for(i=1;i<res.length-1;i++){
-            res[i]="/"+res[i];
-            var item = document.createElement("li");
-            item.innerHTML=item.innerHTML+res[i];
-            path="";
-            for(j=1;j<=i;j++){
-                path=path+res[j];
-            }
-            pathArr[i]=path;
-            item.addEventListener("click" , (e)=>
-            {
-                str=e.target.textContent;
-                initFolders(pathArr[res.indexOf(str)]);
-                
+                        lastSelRow.style.opacity=1;
+                        lastSelRow.style.border="black";
+                        lastSelRow.style.backgroundColor="white";
+                    }
+                    var onsel=document.getElementById("onselect");
+                    onsel.style.opacity=1.0;
+                    row.style.border="grey 3px solid";
+                    row.style.backgroundColor="rgba(190, 190, 190, 0.692)";
+                    fileName=e.target.textContent;
+                    selectedFilePath=currentPath+fileName;
+                    lastSelRow=row;  
+                    getInfo();  
+                }); 
+                row.tabIndex=0; 
+                nm.textContent=element.attributes.name;
+                mod.textContent=months[element.attributes.modified.substring(6,7)-1]+
+                " "+element.attributes.modified.substring(8,10)+
+                ", "+element.attributes.modified.substring(0,4);
+                row.appendChild(nm);
+                row.appendChild(mod);            
+                table.appendChild(row);
             });
-            dirList.appendChild(item);
+        }
+        if(table.children.length==1){
+            empty="true";
+            var row=document.createElement("tr");                   
+            nm = document.createElement("td"); 
+            nm.textContent="The folder is empty";
+            nm.colSpan=2;
+            row.appendChild(nm);
+            table.appendChild(row);
+            row.addEventListener("contextmenu", (e)=>
+            {
+                fileName=e.target.textContent;
+                selectedFilePath=currentPath+fileName;
+                e.preventDefault();
+                showContextMenu();
+                cm=document.getElementById("contextMenu");
+                cm.style.top=e.clientY;
+                cm.style.left=e.clientX;
+            });   
+        }
+        folderTable.appendChild(table);
+    });
+    var dirList=document.getElementById("dirList");
+    dirList.innerHTML="";
+    var res = currentPath.split("/");  
+    var path;
+    var str="";
+    var pathArr=[];
+    for(i=1;i<res.length;i++)
+    {
+        res[i]="/"+res[i];
+        var item = document.createElement("li");
+        item.innerHTML=item.innerHTML+res[i];
+        path="";
+        for(j=1;j<=i;j++)
+        {
+            path=path+res[j];
+        }
+        pathArr[i]=path;
+        item.addEventListener("click" , (e)=>
+        {
+            str=e.target.textContent;
+            initFolders(pathArr[res.indexOf(str)]);       
+        });
+        dirList.appendChild(item);
     }
+}
+window.addEventListener("keydown", function(e)
+{
+    if ( e.keyCode == 8 )
+    {
+        back();
     }
-    window.addEventListener("click", (e)=>
+});
+window.addEventListener("click", (e)=>
     { 
         showContextMenu(false);
         var onsel=document.getElementById("onselect"); 
@@ -131,7 +171,6 @@ var months=["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "O
             onsel.style.opacity=0.0;                  
         } 
 });
-
 function back()
 {
     var newPath=currentPath.substring(0,currentPath.length-1);   
@@ -141,13 +180,11 @@ function back()
         initFolders(newPath);  
     } 
 }
-
 function showContextMenu(show=true)
 {
     var cm=document.getElementById("contextMenu");
     cm.style.display=show? "block" : "none";
 }
-
 function popNewFold() {
     document.getElementById("foldName").style.display = "block";
   }
@@ -162,34 +199,18 @@ function newFolder(){
         })
         closePopFolder();
 }
-
-
 function fileUpload()
 {
-    var objFile={
-        "id": "/Capture.PNG",
-        "type": "file",
-        "attributes": {
-            "name": "Capture.PNG",
-            "path": "/Capture.PNG",
-            "readable": 1,
-            "writable": 1,
-            "created": 1477235998,
-            "modified": 1477235998,
-            "height": 128,
-            "width": 128,
-            "size": 15664
-        }
-    }
     var file = document.getElementById("uploadedFile").files[0];
     console.log(file)
     var data = {"mode": "upload",  "path": "/New Folder"};
-    var options ={method: 'POST',
-    body: JSON.stringify(data)
-}
+    var options = 
+    {
+        method: 'POST',
+        body: JSON.stringify(data)
+    }
 fetch('http://localhost:9000/filemanager', options);
 }
-
 function copyItem(){
     moveOrCopy=1;
     fileToPaste=selectedFilePath;
@@ -217,11 +238,15 @@ function pasteItem(){
         alert("No item selected for pasting");
     }
 }
-
 function popRename() {
-    if(empty!="true"){
-    document.getElementById("itemRename").style.display = "block";
-  }}
+    if(empty=="true"){
+        alert("No file selected to rename");
+    }
+    else
+    {
+        document.getElementById("itemRename").style.display = "block";
+    }
+}
 function closePopRename() {
     document.getElementById("itemRename").style.display = "none";
 }
@@ -234,12 +259,16 @@ function renameitem()
         })
         closePopRename();    
 }
-
 function confirmDelete() 
 {
-    if(empty!="true"){
+    if(empty=="true"){
+        alert("No file selected to delete");
+    }
+    else
+    {
         document.getElementById("itemDelete").style.display = "block";
-  }}
+    }
+}
 function closePopDelete() {
     document.getElementById("itemDelete").style.display = "none";
 }
@@ -250,4 +279,12 @@ function deleteItem()
         initFolders(currentPath);
         })
         closePopDelete();
+}
+function getInfo(){
+    fetch("http://localhost:9000/filemanager?mode=getinfo&path="+(selectedFilePath))
+    .then(function(response){
+        var info=response;       
+        
+        console.log(info);
+    })
 }
